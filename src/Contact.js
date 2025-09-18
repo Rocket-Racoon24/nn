f783f347ce3670382091b83abe83c8f08498c739
Rocket-Racoon24 <rocketaime@gmail.com>
2025-09-18 12:11:34 +0530

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Here’s how you can reach us!</p>
+    </div>
+  );
+};
+
+export default Contact;
